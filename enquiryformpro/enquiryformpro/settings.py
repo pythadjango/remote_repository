1346d387ef9a2883f6145f5b771e70344ae6045e
pythadjango <61425809+pythadjango@users.noreply.github.com>
2020-02-25 00:11:05 +0530

    Update settings.py

--- enquiryformpro/enquiryformpro/settings.py
+++ enquiryformpro/enquiryformpro/settings.py
@@ -78,14 +78,10 @@ WSGI_APPLICATION = 'enquiryformpro.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'enquirydb',
-        'USER': 'root',
-        'PASSWORD':'root'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
-
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
 
